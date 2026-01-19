c27f35e9bbff356f980c939d1f7d26576d70f0ac
JCeroOne <jceroone@gmail.com>
2026-01-19 05:52:48 +0000

    Task completed

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 
 if __name__ == "__main__":
     args = len(sys.argv) - 1
-    print("{} argument{}{}".format(args, "" if args == 1 else "s"), end="")
+    print("{} argument{}".format(args, "" if args == 1 else "s"), end="")
     print("{}".format("." if args == 0 else ":"))
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
