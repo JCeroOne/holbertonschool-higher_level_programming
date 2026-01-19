60a0ca16ab187b5874b1deb9f854b0dec61d2f20
JCeroOne <jceroone@gmail.com>
2026-01-19 05:48:35 +0000

    Task completed

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args = len(sys.argv) - 1
+    print("{} argument{}".format(args, "" if args == 1 else "s"))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
