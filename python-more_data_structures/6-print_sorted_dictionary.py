785602c79afd2bd3ad1d95540ccf03d04a7004c0
JCeroOne <jceroone@gmail.com>
2026-01-26 04:47:23 +0000

    Patch

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    if type(a_dictionary) is not dict:
+        return
     k = sorted(a_dictionary.keys())
     for el in k:
         print("{}: {}".format(el, a_dictionary[el]))
