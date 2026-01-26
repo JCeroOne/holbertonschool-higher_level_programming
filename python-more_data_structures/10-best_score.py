af2ba18baf2091d7f57df57479da92b10dbefad6
JCeroOne <jceroone@gmail.com>
2026-01-26 02:25:34 +0000

    Patch

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 def best_score(a_dictionary):
     max_score = None
     for k in a_dictionary:
-        if a_dictionary[key] > a_dictionary[max_score] or not max_score:
+        if a_dictionary[k] > a_dictionary[max_score] or not max_score:
             max_score = k
     return max_score
