83b9a875c63ad5fba956b32d4c7b442fadeee824
JCeroOne <jceroone@gmail.com>
2026-02-01 20:17:26 +0000

    Task completed

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Define a square class with a private, instance attribute named size."""
+
+class Square:
+    """Represents a square."""
+    
+    def __init__(self, size):
+        self.__size = size;
+
