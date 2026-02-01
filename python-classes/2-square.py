575c65c3e21a6cd3a29e6b0f1b01d0901cddafd5
JCeroOne <jceroone@gmail.com>
2026-02-01 20:24:58 +0000

    Task completed

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Define a square class with a private, instance attribute named size.
+'size' must be an integer, and greater or equal to 0."""
+
+class Square:
+    """Represents a square."""
+    
+    def __init__(self, size=0):
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        
+        self.__size = size;
+
