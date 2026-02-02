4ddbcc0895773abe0ad2c81846fb33a6727078d1
JCeroOne <jceroone@gmail.com>
2026-02-02 05:08:58 +0000

    Patch

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,8 +6,9 @@
 class Square:
     """Represents a square."""
     
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         self.__size = size
+        self.__position = position
 
     @property
     def size(self):
