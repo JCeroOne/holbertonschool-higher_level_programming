1bcdaf147a8bec0a3c9f83bfb42d40663d6abde4
JCeroOne <jceroone@gmail.com>
2026-02-02 05:22:27 +0000

    Task completed

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Defines an empty Rectangle class. Nothing else."""
+
+class Rectangle:
+    """Currently does absolutely nothing"""
+    pass
+
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+MORE Python classes. You read that right, and I believe there's not much else to say.
