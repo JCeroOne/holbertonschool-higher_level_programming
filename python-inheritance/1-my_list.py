5f73e4ece1dba1d22b93ae30f2d6b10bdbb09195
JCeroOne <jceroone@protonmail.com>
2026-02-08 23:37:45 +0000

    Patch

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
 """MyList - Provides a single method."""
+
 
 class MyList(list):
     """MyList - A class that expands the built-in list class."""
-    
+
     def print_sorted(self):
         """print_sorted - Prints the list, sorted in ascending order."""
-
         print(sorted(self))
