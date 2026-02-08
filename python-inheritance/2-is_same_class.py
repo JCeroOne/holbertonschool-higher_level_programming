b80d6b76f2cbdd078eee014accb982f0b8f6fb48
JCeroOne <jceroone@protonmail.com>
2026-02-08 23:56:28 +0000

    Patch

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -11,4 +11,4 @@ def is_same_class(obj, a_class):
     Returns:
         True if the object is an instance of the class
         False otherwise"""
-    return type(obj) == a_class
+    return type(obj) is a_class
