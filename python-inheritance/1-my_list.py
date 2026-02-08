fea679b1236e663c48137d402cf9480bc8db90fc
JCeroOne <jceroone@protonmail.com>
2026-02-08 23:32:59 +0000

    Patch

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,10 +5,7 @@
 class MyList(list):
     """MyList - A class that expands the built-in list class."""
     
-    def __init__(self):
-        super().__init__()
-
     def print_sorted(self):
         """print_sorted - Prints the list, sorted in ascending order."""
 
-        print(self.sort())
+        print(sorted(self))
