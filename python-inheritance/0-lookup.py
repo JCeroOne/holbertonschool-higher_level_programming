4e3519c1339525b46bb5ea70084fa18265296978
JCeroOne <jceroone@protonmail.com>
2026-02-08 23:07:08 +0000

    Patch

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,4 +4,10 @@
 """Provides a lookup function, which lists the methods and attributes of an object"""
 
 def lookup(obj):
+    """
+    Parameters:
+        obj - The object from which to get methods and attributes
+
+    Returns: A list containing the methods and attributes of the object
+    """
     return obj.__dict__.keys()
