398a138cda140ef1ec35993e3a80bf4aefe1d549
JCeroOne <jceroone@protonmail.com>
2026-02-09 00:00:27 +0000

    Task completed

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Provides the 'is_kind_of_class' function."""
+
+
+def is_same_class(obj, a_class):
+    """Checks if an object is an instance of the class, or a subclass.
+    Parameters:
+        obj - The object to check
+        a_class - The class to compare the object to
+
+    Returns:
+        True if the object is an instance of the class or a subclass
+        False otherwise"""
+    return isinstance(obj, a_class)
