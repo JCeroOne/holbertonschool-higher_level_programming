dd3d6aac616fb9becf67242040b534062a750638
JCeroOne <jceroone@protonmail.com>
2026-02-09 00:43:56 +0000

    Task completed

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Provides the 'BaseGeometry' and 'Rectangle' classes."""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A representation of a Rectangle in Python."""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
