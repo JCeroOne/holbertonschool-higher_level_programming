cc074a7ef53e746c0609e947e15c4f759ff842f0
JCeroOne <jceroone@protonmail.com>
2026-02-09 00:16:44 +0000

    Task completed

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Provides the 'BaseGeometry' class."""
+
+
+class BaseGeometry:
+    """Apparently, the base of many future subclasses."""
+
+    def area(self):
+        """Will calculate the area of the geometry in subclasses."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates whether <name> is a positive integer.
+        Parameters:
+            name - The name of the numeric value
+            value - The value of <name>"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
