79c2bc205a9aef97e981c26d538de9ff4974e735
JCeroOne <jceroone@protonmail.com>
2026-02-09 00:51:09 +0000

    Task completed

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Provides a 'Square' class."""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square in Python."""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        self.__width = size
+        self.__height = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
