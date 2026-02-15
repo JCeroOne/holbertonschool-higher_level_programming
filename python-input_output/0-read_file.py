c7d6b6afd216e4de617ccc5537b96031e1f7a830
root <root@ip-10-42-159-67.ec2.internal>
2026-02-15 19:07:30 +0000

    Task completed

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defines the read_file function."""
+
+
+def read_file(filename=""):
+    """Reads a file and prints its contents to the console.
+
+    Parameters:
+        filename: The name of the file to read."""
+
+    with open(filename, encoding="utf-8") as f:
+        data = f.read()
+        print(data)
