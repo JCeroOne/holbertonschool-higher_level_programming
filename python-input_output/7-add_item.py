11fd8ff6a2a937c0b176cfd489d084cb37fbd768
JCeroOne <jceroone@gmail.com>
2026-02-16 05:32:04 +0000

    Task completed

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Adds the given arguments to a JSON array."""
+
+import sys
+import json
+
+save_to_json_file = __import__("5-save-to-json-file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+try:
+    items = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    items = []
+
+items.extend(sys.argv[1:])
+
+save_to_json_file(items, "add_item.json")
