8fcde20010db6673d615f4b564afe92f4c59b897
JCeroOne <jceroone@gmail.com>
2026-02-16 05:41:54 +0000

    Task completed

---
+++ python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Creates the Student class."""
+
+
+class Student:
+    """A Python representation of a student."""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a simplified, JSON-ready version of the student."""
+        if attrs == None:
+            return self.__dict__.copy()
+        return {k: v for k, v in self.__dict__.items() if k in attrs}
