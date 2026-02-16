cfe8e05f55e11f702a2cfbae5d29c26df71ae7ce
JCeroOne <jceroone@gmail.com>
2026-02-16 05:18:39 +0000

    Task completed

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines the from_json_string function."""
+
+import json
+
+
+def from_json_string(my_str):
+    """Converts the JSON string to an object.
+
+    Parameters:
+        my_str: The JSON string to convert.
+
+    Returns: The converted object."""
+    return json.loads(my_obj)
