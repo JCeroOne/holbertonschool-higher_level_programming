a9531d5d7e1b4277c0369ad3907e1a8232fe21a7
JCeroOne <jceroone@gmail.com>
2026-02-16 05:14:33 +0000

    Task completed

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+
+"""Defines the to_json_string function."""
+
+
+def to_json_string(my_obj):
+    """Converts the object to a JSON string.
+
+    Parameters:
+        my_obj: The object to convert.
+
+    Returns: A JSON representation of the object."""
+    return json.dumps(obj)
