c1fb771114587ef99ebcf0701693102944e953b7
JCeroOne <jceroone@gmail.com>
2026-02-16 05:10:10 +0000

    Task completed

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Defines the append_write function."""
+
+
+def append_write(filename="", text=""):
+    """Adds the given text to the end of the file, creates it if needed.
+
+    Parameters:
+        filename: The file to append to, or create.
+        text: The text to append to the file.
+
+    Returns: The number of characters written to the file."""
+    
+    with open(filename, "a", encoding="utf-8"):
+        return f.write(text)
