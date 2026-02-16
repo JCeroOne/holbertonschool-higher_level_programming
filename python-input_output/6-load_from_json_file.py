66bb7b3501e88fbc1e01fdb4610789e371c60ac9
JCeroOne <jceroone@gmail.com>
2026-02-16 05:25:57 +0000

    Task completed

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines the load_from_json_file function."""
+
+import json
+
+
+def load_from_json_file(filename):
+    """Loads a JSON string from a file and converts it to an object.
+
+    Parameters:
+        filename: The file to read from.
+
+    Returns: The converted object."""
+
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.dumps(f.read())
