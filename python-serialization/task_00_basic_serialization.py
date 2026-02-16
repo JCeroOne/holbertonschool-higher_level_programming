41bf4ac6891dfc88fcc6b0bb91e02e7838d027ed
JCeroOne <jceroone@gmail.com>
2026-02-16 05:57:55 +0000

    Task completed

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,9 +5,10 @@ import json
 def serialize_and_save_to_file(data, filename):
     with open(filename, "w", encoding="utf-8") as f:
         f.write(json.dumps(data))
+    print(f"Data serialized and saved to {filename}")
 
 def load_and_deserialize(filename):
     d = None
     with open(filename, "r", encoding="utf-8"):
         d = f.read()
-    return json.loads(d)
+    print("Deserialized data:\n{}".format(json.loads(d)))
