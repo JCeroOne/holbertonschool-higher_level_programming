b78de157dd7cc52d08592c58b8ca66d1f47b7b8a
JCeroOne <jceroone@gmail.com>
2026-02-17 18:34:20 +0000

    Task completed

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines the convert_csv_to_json function."""
+
+import csv
+import json
+
+
+def convert_csv_to_json(filename):
+    try:
+        d = []
+        with open(filename, "r", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                d.append(row)
+
+        with open("data.json", "w", encoding="utf-8") as f:
+            json.dump(data, f)
+
+        return True
+    except (
+            FileNotFoundError,
+            Exception
+        ):
+        return False
