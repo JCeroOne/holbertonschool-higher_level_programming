93f6f903d5a6b1b32396c25824aa0fba300172d2
JCeroOne <jceroone@gmail.com>
2026-02-22 01:46:04 +0000

    Patch

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -38,7 +38,7 @@ class SimpleHTTPHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 def run(server=http.server.HTTPServer, handler=SimpleHTTPHandler, port=8000):
     httpd = server(("", port), handler)
