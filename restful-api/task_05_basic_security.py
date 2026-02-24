6a8bf2b946548545aee1939ec279bbd1d3aa282e
JCeroOne <jceroone@gmail.com>
2026-02-24 18:43:57 +0000

    Patch

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -91,4 +91,4 @@ def fresh_needed(header, payload):
     return jsonify({"error": "Fresh token required"}), 401
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000)
+    app.run(host="127.0.0.1", port=5000)
