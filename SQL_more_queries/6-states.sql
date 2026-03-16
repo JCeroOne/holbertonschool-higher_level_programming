2a7366a432999b32014c51e7a81528057b87f065
JCeroOne <jceroone@gmail.com>
2026-03-16 03:39:53 +0000

    Patch

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -5,7 +5,5 @@ USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS states (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES states(id)
 );
