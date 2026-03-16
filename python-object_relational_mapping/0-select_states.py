fc9f189da520f0cbbefb92288ef3b2d8b6c4a40b
JCeroOne <jceroone@gmail.com>
2026-03-16 04:22:00 +0000

    Task completed

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,22 +6,26 @@ import sys
 
 
 if __name__ == "__main__":
-    """Connect to MySQL and retrieve states."""
+    """Connects to MySQL and prints all states ordered by id."""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=database
     )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    rows = cursor.fetchall()
+    states = cursor.fetchall()
 
-    for row in rows:
-        print(row)
+    for state in states:
+        print(state)
 
     cursor.close()
     db.close()
