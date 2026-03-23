fc0cc27f2556d908e4c39b70868577010714f8f3
JCeroOne <jceroone@gmail.com>
2026-03-23 02:17:37 +0000

    Task completed

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 const args = process.argv.length;
-if(args == 0) console.log('No argument');
-else if(args == 1) console.log('Argument found');
-else console.log('Arguments found');
+
+if (args == 0) {
+	console.log('No argument');
+}
+else if (args == 1) {
+	console.log('Argument found');
+}
+else {
+	console.log('Arguments found');
+}
