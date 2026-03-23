63719d0f047ee1e080bb7bf4cb244b3c5eafb8a7
JCeroOne <jceroone@gmail.com>
2026-03-23 04:10:45 +0000

    Patch

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-let args = process.argv;
-
-args.splice(0, 2).sort((a, b) => Number(b) - Number(a));
-console.log(args[1]);
+let args = [...process.argv];
+args.splice(0, 2);
+if (args.length < 2) {
+  console.log(0);
+} else {
+  args.sort((a, b) => Number(b) - Number(a));
+  console.log(args[1]);
+}
