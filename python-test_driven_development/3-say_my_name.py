0e2c39cffb2931112b27b7c246b84b023be8c569
JCeroOne <jceroone@gmail.com>
2026-06-16 20:13:47 +0000

    Update

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -14,9 +14,9 @@ def say_my_name(first_name, last_name=''):
     fnExc = "first_name must be a string"
     lnExc = "last_name must be a string"
 
-    if type(first_name) != str:
+    if type(first_name) is not str:
         raise TypeError(fnExc)
-    if type(last_name) != str:
+    if type(last_name) is not str:
         raise TypeError(lnExc)
 
     print("My name is {} {}".format(first_name, last_name))
