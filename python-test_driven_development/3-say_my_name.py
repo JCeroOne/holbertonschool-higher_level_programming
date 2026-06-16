48e4b2b0769b41b6a1eb64142e6a930efce400da
JCeroOne <jceroone@gmail.com>
2026-06-16 20:09:45 +0000

    Update

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -19,8 +19,7 @@ def say_my_name(first_name, last_name=''):
     if type(last_name) != str:
         raise TypeError(lnExc)
 
-    print("My name is {}".format(first_name), end='')
-    if len(last_name) == 0:
-        print()
-    else:
-        print(" {}".format(last_name))
+    statement = "My name is {}".format(first_name)
+    if(len(last_name) > 0):
+        statement += " {}".format(last_name)
+    print(statement)
