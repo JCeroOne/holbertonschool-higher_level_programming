c9602c4e70157c3dd575baa0c787a1fbb388c315
JCeroOne <jceroone@gmail.com>
2026-06-16 17:15:14 +0000

    Update

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,10 +6,9 @@ def safe_print_list_integers(my_list=[], x=0):
             try:
                 print("{:d}".format(my_list[i]), end="")
                 printed += 1
-            except:
+            except Exception:
                 continue
         print()
     except Exception:
         print()
     return printed
-
