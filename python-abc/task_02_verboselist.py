aead6ba03c63f2f2999879ae6787ca23c38970a8
JCeroOne <jceroone@gmail.com>
2026-06-16 21:21:17 +0000

    Update

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -6,20 +6,17 @@ class VerboseList(list):
 
     def append(self, item):
         super().append(item)
-        print(f"Added [{item}] to the list")
+        print(f"Added [{item}] to the list.")
 
     def extend(self, items):
         super().extend(items)
-        print(f"Extending the list with [{len(items)}] items.")
+        print(f"Extended the list with [{len(items)}] items.")
 
     def remove(self, item):
         print(f"Removed [{item}] from the list.")
         super().remove(item)
 
     def pop(self, index=None):
-        if index is None:
-            value = super().pop()
-            print(f"Popped [{value}] from the list.")
-        else:
-            value = super().pop(index)
-            print(f"Popped [{value}] from the list.")
+        value = super().pop(index)
+        print(f"Popped [{value}] from the list.")
+        return value
