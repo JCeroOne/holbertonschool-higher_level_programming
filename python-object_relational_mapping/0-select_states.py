abef19fb98d68ae1561772ac17702362afed8f27
JCeroOne <jceroone@gmail.com>
2026-06-16 21:29:31 +0000

    Update

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,34 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa."""
+"""List all states from the hbtn_0e_0_usa database.
+"""
+
+import sys
 
 import MySQLdb
-import sys
+
+
+def fetch_states(user, password, db_name):
+    """Connect to MySQL and return rows from states ordered by id."""
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=user, passwd=password, db=db_name)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    cur.close()
+    db.close()
+    return rows
+
+
+def main():
+    """Read command-line arguments and print each state row as a tuple."""
+    if len(sys.argv) != 4:
+        return
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    for row in fetch_states(user, password, db_name):
+        print(row)
 
 
 if __name__ == "__main__":
-    """Connects to MySQL and prints all states ordered by id."""
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    states = cursor.fetchall()
-
-    for state in states:
-        print(state)
-
-    cursor.close()
-    db.close()
+    main()
