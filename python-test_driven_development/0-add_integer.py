cfe465eb55a3ffd458f3d06ede28e4ba657583da
JCeroOne <jceroone@gmail.com>
2026-06-16 17:28:41 +0000

    Update

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,7 +2,20 @@
 """
 Defines add_integer, a function that adds two integers together.
 """
+
+
 def add_integer(a, b=98):
+    """
+    Adds two integers and returns the result.
+
+    Parameters:
+        a (int): The first integer
+        b (int): The second integer
+
+    Returns:
+        (int) The addition result.
+    """
+
     if type(a) is float:
         a = int(a)
     else if type(a) is not int:
